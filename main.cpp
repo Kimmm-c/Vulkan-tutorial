c15e73476973533fd06e76f0ce7bfcae7a16f95c
Kimmm-c <97310663+Kimmm-c@users.noreply.github.com>
2024-09-27 14:15:38 -0700

    implemented image views for swap chain images

--- main.cpp
+++ main.cpp
@@ -100,6 +100,7 @@ class TriangleApp {
     // Swap chain and its settings
     VkSwapchainKHR swapChain;
     vector<VkImage> swapChainImages;
+    vector<VkImageView> swapChainImageViews;
     VkFormat swapChainImageFormat;
     VkExtent2D swapChainExtent;
 
@@ -167,6 +168,41 @@ private:
         createLogicalDevice();
 
         createSwapChain();
+        createImageView();
+    }
+
+    /**
+     * @brief Create an image view, which describes how to access an image and which part to access.
+     */
+    void createImageView() {
+        uint8_t imageCount = swapChainImages.size();
+        swapChainImageViews.resize(imageCount);
+
+        for (size_t i = 0; i < imageCount; i++) {
+            VkImageViewCreateInfo createInfo{};
+            createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+            createInfo.image = swapChainImages[i];
+
+            // Specify how image data should be interpreted.
+            createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+            createInfo.format = swapChainImageFormat;
+
+            createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+
+            // Specify the image's purpose and which part of the image should be accessed.
+            createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT; // Access the color portion of the image
+            createInfo.subresourceRange.baseMipLevel = 0;                       // mip = 0 means the full resolution image
+            createInfo.subresourceRange.levelCount = 1;                         // the number of mipmap levels accessible from the image
+            createInfo.subresourceRange.baseArrayLayer = 0;                     // The starting layer, in this case, starting from the first layer.
+            createInfo.subresourceRange.layerCount = 1;                         // the number of layers
+
+            if (vkCreateImageView(logicalDevice, &createInfo, nullptr, &swapChainImageViews[i]) != VK_SUCCESS) {
+                throw runtime_error("Failed to create image views");
+            }
+        }
     }
 
     void createSwapChain() {
@@ -610,6 +646,9 @@ private:
             DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
         }
 
+        for (auto imageView : swapChainImageViews) {
+            vkDestroyImageView(logicalDevice, imageView, nullptr);
+        }
         vkDestroySwapchainKHR(logicalDevice, swapChain, nullptr);
         vkDestroyDevice(logicalDevice, nullptr);
         vkDestroySurfaceKHR(instance, surface, nullptr);
